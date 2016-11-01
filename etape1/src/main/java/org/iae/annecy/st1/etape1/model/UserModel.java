68fe3b83cca8264d613031092e08a8b326759d12
jderuette <jde@webmecanik.com>
2016-11-01 17:06:26 +0100

    Renomage path des modeles

--- etape1/src/main/java/org/iae/annecy/st1/etape1/model/UserModel.java
+++ etape1/src/main/java/org/iae/annecy/st1/etape1/model/UserModel.java
@@ -4,18 +4,18 @@ import org.iae.annecy.st1.common.mvc.Controller;
 import org.iae.annecy.st1.common.mvc.DataView;
 import org.iae.annecy.st1.common.mvc.Model;
 
-public class UserModel implements Model{
+public class UserModel implements Model {
 
-	public DataView get(){
+	public DataView get() {
 		DataView datas = new UserData();
-		
+
 		datas.add("name", "jérémie");
-		
+
 		return datas;
 	}
 
 	public String getPath() {
-		return "user_display";
+		return "user:display";
 	}
 
 	public void register(Controller controller) {
